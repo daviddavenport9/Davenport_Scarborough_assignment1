0a1fa497bffb0f3daf48fb996e839f8469d80ad9
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-02 18:00:53 -0400

    Create Instructor.cpp

---
+++ Instructor.cpp
@@ -0,0 +1,26 @@
+#include "Instructor.h"
+
+
+bool Instructor::login(string username, string password){
+
+}
+
+string Instructor::getInstructorName(){
+
+}
+
+Student Instructor::getStudent(string username){
+
+}
+
+Student Instructor::getMinStudent(int gradeType){
+
+}
+
+Student Instructor::getMaxStudent(int gradeType){
+
+}
+
+double Instructor::getAvg(int gradeType){
+
+}
