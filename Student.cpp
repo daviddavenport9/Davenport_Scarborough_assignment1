71c89349de9861999d8c0a43c581aa232337a5b1
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-02 18:23:15 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,29 +1,49 @@
 #include "Student.h"
 
 bool Student::login(string username, string password){
-
+  return true;
 }
 
 string Student::getStudentName(){
-
+  return fullName;
 }
 
 int Student::getProjectGrade(){
-
+  return projectGrade;
 }
 
 int Student::getQuizGrade(){
-
+  return quizGrade;
 }
 
 int Student::getMidtermGrade(){
-
+  return midtermGrade;
 }
 
 int Student::getFinalGrade(){
-
+  return finalGrade;
 }
 
 double Student::getOverallGrade(){
+  return ((projectGrade * 0.30) + (quizGrade * 0.10) + (midtermGrade * 0.20) + (finalGrade * 0.40));
+}
 
+void Student::setStudentName(string fullName){
+  this->fullName = fullName;
+}
+
+void Student::setProjectGrade(int grade){
+  projectGrade = grade;
+}
+
+void Student::setQuizGrade(int grade){
+  quizGrade = grade;
+}
+
+void Student::setMidtermGrade(int grade){
+  midtermGrade = grade;
+}
+
+void Student::setFinalGrade(int grade){
+  finalGrade = grade;
 }
