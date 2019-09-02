e4f258c1d3554a438bebd0e4813bdd08c24f35a4
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-02 18:02:11 -0400

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,29 @@
+#include "Student.h"
+
+bool Student::login(string username, string password){
+
+}
+
+string Student::getStudentName(){
+
+}
+
+int Student::getProjectGrade(){
+
+}
+
+int Student::getQuizGrade(){
+
+}
+
+int Student::getMidtermGrade(){
+
+}
+
+int Student::getFinalGrade(){
+
+}
+
+double Student::getOverallGrade(){
+
+}
