1bfccd89a18e82c0f952b463eb0142c906b0695f
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-02 22:52:09 -0400

    Update Instructor.cpp

--- Instructor.cpp
+++ Instructor.cpp
@@ -2,15 +2,30 @@
 
 
 bool Instructor::login(string username, string password){
-
+  string usernameInstruct, passwordInstruct, firstName, lastName;
+  ifstream file("instructors.txt");
+   if (file.fail()) {
+    cerr << "Unable to open file." << endl;
+    exit(1);
+  }
+  while (file >> usernameInstruct >> passwordInstruct >> firstName >> lastName)
+  {
+  if ((usernameInstruct == username) && (passwordInstruct ==  password))
+    {
+      fullName = firstName + " " + lastName;
+      return true;
+    }//if
+  }//while
+  cout << "Login as instructor failed" << endl;
+  return false;
 }
 
 string Instructor::getInstructorName(){
-
+  return fullName;
 }
 
 Student Instructor::getStudent(string username){
-
+  Student student;
 }
 
 Student Instructor::getMinStudent(int gradeType){
@@ -23,4 +38,8 @@ Student Instructor::getMaxStudent(int gradeType){
 
 double Instructor::getAvg(int gradeType){
 
+}
+
+void Instructor::setInstructorName(string fullName){
+  this->fullName = fullName;
 }
