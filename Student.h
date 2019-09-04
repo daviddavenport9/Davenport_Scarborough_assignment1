566c1f6810d8fda0ae80b4054561105c0fd28a3e
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-09-03 22:04:13 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,31 +1,40 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 #include <iostream>
+#include <fstream>
 #include <string.h>
 #include <string>
 
 using namespace std;
 class Student {
 
-public:
-bool login(string username, string password);
-string getStudentName();
-int getProjectGrade();
-int getQuizGrade();
-int getMidtermGrade();
-int getFinalGrade();
-double getOverallGrade();
-
-private:
-
-string fullName;
-int projectGrade;
-int quizGrade;
-int midtermGrade;
-int finalGrade;
+ public:
+  bool login(string username, string password);
+  string getStudentName();
+  int getProjectGrade();
+  int getQuizGrade();
+  int getMidtermGrade();
+  int getFinalGrade();
+  double getOverallGrade();
+  void setStudentName(string fullName);
+  void setProjectGrade(int grade);
+  void setQuizGrade(int grade);
+  void setMidtermGrade(int grade);
+  void setFinalGrade(int grade);
+  void setOverallGrade(int grade);
 
 
+ private:
+
+  string fullName;
+  int projectGrade;
+  int quizGrade;
+  int midtermGrade;
+  int finalGrade;
+  int overallGrade;
 
 
-}; //Student
+
+}; //Student                                                                                                                                                                                         
 #endif
+
