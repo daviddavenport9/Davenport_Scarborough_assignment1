d1f0f2abcbdd3b75caa1593c45e9ad474c1aa1fb
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-17 14:03:28 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,73 +2,72 @@
 
 bool Student::login(string username, string password){
   string usernameStudent, passwordStudent, firstName, lastName;
-  ifstream file("students.txt");
+  ifstream file("students.txt"); // open file
    if (file.fail()) {
     cerr << "Unable to open file." << endl;
     exit(1);
-  }
-  while (file >> usernameStudent >> passwordStudent >> firstName >> lastName >> projectGrade >> quizGrade >> midtermGrade >> finalGrade)
+  } // if
+  while (file >> usernameStudent >> passwordStudent >> firstName >> lastName >> projectGrade >> quizGrade >> midtermGrade >> finalGrade) // get each token
   {
-  if ((usernameStudent == username) && (passwordStudent ==  password))
-    {
-      fullName = firstName + " " + lastName;
+    if ((usernameStudent == username) && (passwordStudent ==  password)){ // if valid login
+      fullName = firstName + " " + lastName; // concatenate name
       return true;
     }//if
   }//while
   cout << "Login as Student failed" << endl;
   return false;
-}
+} // login
 
 string Student::getStudentName(){
   return fullName;
-  }
+} // getStudentName
   
 int Student::getProjectGrade(){
   return projectGrade;
-}
+} // getProjectGrade
 
 int Student::getQuizGrade(){
   return quizGrade;
-}
+} // getQuizGrade
 
 int Student::getMidtermGrade(){
   return midtermGrade;
-}
+} // getMidtermGrade
 
 int Student::getFinalGrade(){
   return finalGrade;
-}
+} // getFinalGrade
 
 double Student::getOverallGradeCalc(){
   overallGrade = ((projectGrade * 0.30) + (quizGrade * 0.10) + (midtermGrade * 0.20) + (finalGrade * 0.40));
   return overallGrade;
-}
+} // getOverallGradeCalc
 
 double Student::getOverallGrade(){
   return overallGrade;
-}
+} // getOverallGrade
 
 
 void Student::setStudentName(string fullName){
   this->fullName = fullName;
-}
+} // setStudentName
 
 void Student::setProjectGrade(int grade){
   projectGrade = grade;
-}
+} // setProjectGrade
 
 void Student::setQuizGrade(int grade){
   quizGrade = grade;
-}
+} // setQuizGrade
 
 void Student::setMidtermGrade(int grade){
   midtermGrade = grade;
-}
+} // setMidtermGrade
 
 void Student::setFinalGrade(int grade){
   finalGrade = grade;
-}
+} // setFinalGrade
 
 void Student::setOverallGrade(double term){
   overallGrade = term;
-}
+} // setOverallGrade
